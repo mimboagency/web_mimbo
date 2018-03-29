15692a7065437c885ead1f077783911a3566c2db
mimboagency <37906953+mimboagency@users.noreply.github.com>
2018-03-29 20:53:35 +0300

    Доработка js
    Добавление анимации в js

--- js/script.js
+++ js/script.js
@@ -24,8 +24,91 @@ $(".openmenu").click(function(){
  })
 
  
+ 
+ 
 $( document ).ready(function() {
 	
+	
+    //fullpage.js
+    $('body').fullpage({
+      scrollBar: true,
+	  fitToSectionDelay:300,
+      //navigation: false,
+     // navigationTooltips: ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4'],
+      loopBottom: false,
+      sectionSelector: '.blocks1,.block2,.block3, .block4',
+	//  scrollOverflow:true,
+	  normalScrollElements: '.block4,.block5, .block6, .block7, .block8, .block9',
+	 fitToSection: false
+    });
+	
+	function goanimation(obj,delay){
+	obj.addClass('wow');
+	obj.addClass('fadeInUp');
+	obj.attr( "data-wow-offset", 150 );
+	obj.attr( "data-wow-duration", "2.5s" );
+	obj.attr( "data-wow-delay",  delay + "s" );
+	}
+	
+	function goanimation2(obj,delay){
+	obj.addClass('wow');
+	obj.addClass('fadeInDown');
+	obj.attr( "data-wow-offset", 150 );
+	obj.attr( "data-wow-duration", "2.5s" );
+	obj.attr( "data-wow-delay",  delay + "s" );
+	}
+	
+	function goanimation3(obj,delay){
+	obj.addClass('wow');
+	obj.addClass('fadeInDownOP');
+	obj.attr( "data-wow-offset", 150 );
+	obj.attr( "data-wow-duration", "2s" );
+	obj.attr( "data-wow-delay",  delay + "s" );
+	}
+	
+	function goanimation4(obj,delay){
+	obj.addClass('wow');
+	obj.addClass('animate-text');
+	obj.attr( "data-wow-offset", 150 );
+	obj.attr( "data-wow-duration", "2s" );
+	obj.attr( "data-wow-delay",  delay + "s" );
+	}
+	
+	goanimation($('.whiteline1'),0);
+	goanimation2($('.whiteline2'),0.3);
+	goanimation($('.greyline1'),0);
+	goanimation2($('.greyline2'),0.3);			
+	goanimation2($('.greyline3'),0);	
+	goanimation2($('.greyline4'),0);
+	goanimation($('.greyline5'),0);
+	goanimation2($('.greyline6'),0);
+	goanimation2($('.greyline7'),0);
+	goanimation2($('.greyline8'),0);
+	goanimation($('.greyline9'),0);
+	goanimation($('.greyline00copy'),0);
+	goanimation($('.greyline0'),0.3);	
+	goanimation($('.greyline00'),0);
+
+	goanimation3($('h2'),0);
+
+	
+	WOW.prototype.addBox = function(element) {
+    this.boxes.push(element);
+	};
+
+	  // Init WOW.js and get instance
+	  var wow = new WOW();
+	  wow.init();
+
+	  $('.wow').on('scrollSpy:exit', function() {
+		$(this).css({
+		  'visibility': 'hidden',
+		  'animation-name': 'none'
+		}).removeClass('animated');
+		wow.addBox(this);
+	  }).scrollSpy();
+
+   
 	var modules=$(".modules .module .nummod");
 	var lines=$(".whiteline, .greyline0, .greyline00, .greyline00copy");
 	function resize(){
@@ -44,6 +127,10 @@ $( document ).ready(function() {
 			lines.eq(i).css("height", heightline+15+"px");
 			lines.eq(i).css("bottom", -heightline+"px");
 		}
+		/*parent=$(".greyline0").parent();
+		heightline=parent.parent().parent().parent().offset().top+parent.parent().parent().parent().innerHeight()-parent.offset().top-parent.innerHeight();
+		$(".greyline0").css("height", heightline+15+"px");
+		$(".greyline0").css("bottom", -heightline+"px");*/
 		
 		$(".greyline4").css("left", $(".greyline6").offset().left);
 		$(".greyline3").css("left", $(".greyline0").offset().left);
@@ -54,9 +141,16 @@ $( document ).ready(function() {
 			$(".number").css("left", $(".greyline00").offset().left-147);
 			$(".number0").css("left", $(".greyline00").offset().left-154);
 		}
-		else{
+		else if ($(window).outerWidth()>990){
 			$(".number").css("left", $(".greyline00").offset().left-110);
 			$(".number0").css("left", $(".greyline00").offset().left-113);
+		}
+		else if ($(window).outerWidth()>477){
+			$(".number").css("left", $(".greyline00").offset().left-60);
+			$(".number0").css("left", $(".greyline00").offset().left-60);
+		}
+		else{
+			$(".number").attr("style", "");
 		}
 		
 	}
